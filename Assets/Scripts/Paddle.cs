648d219033108b33c8f41e1cd8d58f45b9617a31
Sina Moradian <sinacsharp@gmail.com>
2026-02-18 01:51:06 +0330

    feat: add touch support to the paddle controller (not tested!)

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -12,6 +12,17 @@ public class Paddle : MonoBehaviour {
 
         pos.y = Mathf.Clamp(pos.y, -4f, 4f);
 
-        transform.position = pos;
+        transform.position = Vector3.Lerp(transform.position, pos, 1f);
+
+    #if UNITY_ANDROID || UNITY_IOS
+        Vector2 touch = InputManager.Instance.TouchPosition;
+
+        if (touch != Vector2.zero)
+        {
+            Vector3 worldPos = Camera.main.ScreenToWorldPoint(touch);
+            transform.position = new Vector3(transform.position.x, worldPos.y, 0f);
+        }
+    #endif
+
     }
 }
